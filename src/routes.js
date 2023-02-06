9b2f841c91ce0dbf600e1dffbccb249b112aa81e
Glauber Pereira <109771771+Glauber003@users.noreply.github.com>
2023-02-06 14:48:39 -0300

    atualizado

---
+++ src/routes.js
@@ -0,0 +1,33 @@
+import { Router, json } from "express"
+
+import ProdutoController from "./controllers/ProdutoController.js"
+import UserController from "./controllers/UserController.js"
+import SessionController from "./controllers/SessionController.js"
+import AnimalController from "./controllers/AnimalController.js"
+import authMiddlewars from "./middlewares/auth.js"
+
+const routes = new Router()
+
+routes.use(json())
+
+routes.post('/user/create', UserController.addUser)
+routes.post('/user/login', SessionController.createSession)
+
+routes.post('/animal/add', AnimalController.addAnimal)
+routes.get('/animal/todos', AnimalController.getAnimal)
+
+routes.use(authMiddlewars)
+
+
+routes.get('/produtos', ProdutoController.getProdutos)
+routes.get('/produto/:id', ProdutoController.getProduto)
+routes.post('/produto/add', ProdutoController.addProduto)
+routes.put('/produto/update', ProdutoController.updateProduto)
+routes.delete('/produto/delete/:id', ProdutoController.deleProduto)
+
+
+
+
+
+
+ export default routes
